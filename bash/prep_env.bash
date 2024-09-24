b27d782e247010765399312e4140111679d67526
pinzu.li <pinzu.li@ailabs.tw>
2024-09-24 04:55:02 +0000

    init

---
+++ bash/prep_env.bash
@@ -0,0 +1,10 @@
+# pip install -U 'xtuner[deepspeed]'
+pip install flash-attn --no-build-isolation
+pip install wandb hf_transfer
+pip install -r requirements.txt
+
+apt update
+apt install vim nvtop tmux -y
+export HF_DATASETS_CACHE="/workspace/.cache/hf/dataset"
+
+wandb login
